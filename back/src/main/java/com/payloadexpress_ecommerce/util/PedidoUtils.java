4c4a8c2d6eb0b718e1a7e06cce8fed7f48e2dd64
Guilherme Kernbichler <guilhermeokern@gmail.com>
2026-08-18 21:25:00 -0300

    feat: cria a classe utilitária

---
+++ back/src/main/java/com/payloadexpress_ecommerce/util/PedidoUtils.java
@@ -0,0 +1,15 @@
+package com.payloadexpress_ecommerce.util;
+
+import java.util.Random;
+
+/**
+ * Cálculos e formatações de apoio ao módulo de pedidos.
+ * Classe utilitária: todos os métodos são estáticos
+ */
+
+public class PedidoUtils {
+
+    private PedidoUtils() {
+        // classe utilitária não deve ser instanciada
+    }
+}
