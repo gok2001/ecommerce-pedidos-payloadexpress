0e3704813ac44703f62d97fb9682dbc9bfc72ee0
Guilherme Kernbichler <guilhermeokern@gmail.com>
2026-08-18 21:27:38 -0300

    feat: adiciona atributos contantes no topo da classe utilitária

--- back/src/main/java/com/payloadexpress_ecommerce/util/PedidoUtils.java
+++ back/src/main/java/com/payloadexpress_ecommerce/util/PedidoUtils.java
@@ -9,6 +9,12 @@ import java.util.Random;
 
 public class PedidoUtils {
 
+    private static final double VALOR_POR_QUILO = 7.50;
+    private static final double FRETE_MINIMO = 15.00;
+    private static final double TAXA_DESCONTO = 0.10;
+    private static final double DESCONTO_MAXIMO = 50.00;
+    private static final double VALOR_FRETE_GRATIS = 300.00;
+
     private PedidoUtils() {
         // classe utilitária não deve ser instanciada
     }
