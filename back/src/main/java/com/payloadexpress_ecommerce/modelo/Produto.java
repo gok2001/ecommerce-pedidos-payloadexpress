30054bacc718ca4acbdd4b711892ba6f0406ef7c
Guilherme Kernbichler <guilhermeokern@gmail.com>
2026-08-25 20:34:17 -0300

    feat: cria classe Produto

--- back/src/main/java/com/payloadexpress_ecommerce/modelo/Produto.java
+++ back/src/main/java/com/payloadexpress_ecommerce/modelo/Produto.java
@@ -2,4 +2,43 @@ package com.payloadexpress_ecommerce.modelo;
 
 public class Produto {
 
+    private String codigo;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidadeEmEstoque;
+    private boolean ativo;
+
+    public Produto(String codigo, String nome, double preco, int quantidadeEmEstoque) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.ativo = true;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public boolean temEstoqueDisponivel(int quantidadeDesejada) {
+        return ativo && quantidadeEmEstoque >= quantidadeDesejada;
+    }
+
+    public void baixarEstoque(int quantidade) {
+        this.quantidadeEmEstoque = this.quantidadeEmEstoque - quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s - R$ %.2f (%d em estoque)", codigo, nome, preco, quantidadeEmEstoque);
+    }
 }
