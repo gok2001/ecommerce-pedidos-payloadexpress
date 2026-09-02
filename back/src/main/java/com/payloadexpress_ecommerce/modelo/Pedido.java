d620e54d84420381f4f500b648ffb90b05221282
GUILHERME ORIGE KERNBICHLER <guilherme.kernbichler@senaisp.edu.br>
2026-09-01 21:30:58 -0300

    feat: adiciona atributos da classe Pedido

--- back/src/main/java/com/payloadexpress_ecommerce/modelo/Pedido.java
+++ back/src/main/java/com/payloadexpress_ecommerce/modelo/Pedido.java
@@ -1,6 +1,13 @@
 package com.payloadexpress_ecommerce.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Classe responsável pela criação de pedidos
 public class Pedido {
-    
+    private String numero;
+    private Cliente cliente = new Cliente();
+    private String data;
+    private String situacao;
+    private List<String> listaDeItens = new ArrayList<>();
 }
