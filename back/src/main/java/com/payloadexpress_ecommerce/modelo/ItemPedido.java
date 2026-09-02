5868f74b9d79f9ddcf117869653254d0288bca72
DAVI LIVINO MAZOTI <davi.mazoti@senaisp.edu.br>
2026-09-01 22:02:30 -0300

    feat: classe ItemPedido criada, junto com seus atributos e metodos

---
+++ back/src/main/java/com/payloadexpress_ecommerce/modelo/ItemPedido.java
@@ -0,0 +1,44 @@
+package com.payloadexpress_ecommerce.modelo;
+
+public class ItemPedido {
+
+    private Produto produto;
+    private long quantidade;
+    private double precoPraticado;
+
+
+
+    public ItemPedido(Produto produto, long quantidade, double precoPraticado){
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoPraticado = precoPraticado;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(long quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoPraticado() {
+        return precoPraticado;
+    }
+
+    public void setPrecoPraticado(double precoPraticado) {
+        this.precoPraticado = precoPraticado;
+    }
+
+    public double calcularSubtotal(){
+        return this.quantidade * this.precoPraticado;
+    }
+}
