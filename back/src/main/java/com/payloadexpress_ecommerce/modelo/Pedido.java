9d02c06a9f47be08eeeda094c29384d6431de296
GUILHERME ORIGE KERNBICHLER <guilherme.kernbichler@senaisp.edu.br>
2026-09-08 22:18:55 -0300

    refactor: adiciona encapsulamento nos setters

--- back/src/main/java/com/payloadexpress_ecommerce/modelo/Pedido.java
+++ back/src/main/java/com/payloadexpress_ecommerce/modelo/Pedido.java
@@ -24,38 +24,53 @@ public class Pedido {
     }
 
     public void setNumero(String numero) {
+        if (numero == null) {
+            throw new IllegalArgumentException("Número não pode ser vazio");
+        }
         this.numero = numero;
     }
-
+    
     public Cliente getCliente() {
         return cliente;
     }
-
+    
     public void setCliente(Cliente cliente) {
+        if (cliente == null) {
+            throw new IllegalArgumentException("Cliente não pode ser vazio");
+        }
         this.cliente = cliente;
     }
-
+    
     public String getData() {
         return data;
     }
-
+    
     public void setData(String data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data não pode ser vazio");
+        }
         this.data = data;
     }
-
+    
     public String situacao() {
         return situacao;
     }
-
+    
     public void setSituacao(String situacao) {
+        if (situacao == null) {
+            throw new IllegalArgumentException("Situação não pode ser vazia");
+        }
         this.situacao = situacao;
     }
-
+    
     public List<ItemPedido> getListaDeItens() {
         return listaDeItens;
     }
-
+    
     public void setListaDeItens(List<ItemPedido> listaDeItens) {
+        if (listaDeItens == null) {
+            throw new IllegalArgumentException("Lista de itens não pode ser vazia");
+        }
         this.listaDeItens = listaDeItens;
     }
 
